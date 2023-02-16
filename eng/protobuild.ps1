d6d3a0066f7fdc67c346e068bb8eb646f534a554
Joshua Brown <acrion@gmail.com>
2023-02-16 15:08:23 -0500

    Swapped protobuf-javascript for ts-proto

--- eng/protobuild.ps1
+++
@@ -1,9 +0,0 @@
-docker build --quiet -t protoc $PWD\protoc
-
-$ProtoDir="../proto"
-$TsDir="../ts/landing/src/Protos"
-
-$ProtoDir = "/" + (Resolve-Path $ProtoDir).Path.Replace(":", "").Replace("\", "/") + ":/work/in"
-$TsDir = "/" + (Resolve-Path $TsDir).Path.Replace(":", "").Replace("\", "/") + ":/work/out"
-
-docker run -t --rm -v $ProtoDir -v $TsDir grpc
